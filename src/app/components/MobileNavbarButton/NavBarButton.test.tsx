05c47b8fffe4bd8d072b5702e556b22ee38a1813
kingjotaro <rafaelleet@gmail.com>
2023-12-06 09:59:08 -0300

    fix(fixing test): fixing test
    fixing test
    
    Mobile Navbar Button #1

--- src/app/components/MobileNavbarButton/NavBarButton.test.tsx
+++ src/app/components/MobileNavbarButton/NavBarButton.test.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { fireEvent, render, waitFor} from '@testing-library/react';
 import NavbarButton from './NavbarButton';
-import {describe, test} from 'vitest'
+import {it} from 'vitest'
 
-describe('NavbarButton', () => {
-  test('deve renderizar o componente', async () => {
+
+  it('deve renderizar o componente', async () => {
      const {getByText, getByAltText   } = render(<NavbarButton />);
      const svgElement = getByAltText('menu SVG');
      fireEvent.click(svgElement)
      const buttonInicio = getByText('Inicio')
 
     });
-  });
+  
 
 
