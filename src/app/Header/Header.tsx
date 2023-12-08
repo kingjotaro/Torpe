dc850c9b254b5949f495646e5bfd738dea0a457c
kingjotaro <rafaelleet@gmail.com>
2023-12-08 14:36:27 -0300

    feat(searchbar): searchbar add
    SearchBar add with regex
    
    SearchBar #4

--- src/app/Header/Header.tsx
+++ src/app/Header/Header.tsx
@@ -2,18 +2,18 @@ import React from 'react'
 import NavbarButton from '../components/MobileNavbarButton/NavbarButton'
 import ButtonLogin from '../components/ButtonLogin/ButtonLogin'
 import ButtonSupport from '../components/ButtonSupport/ButtonSupport'
+import SearchBar from '../components/SearchBar/SearchBar'
 
 export default function Header() {
     return (
-        <nav>
+        <nav >
 
             <div className='flex justify-between bg-purple-100 '>
                 <ButtonLogin/>
                 <ButtonSupport/>
-
                 <NavbarButton />
             </div>
-
+            <SearchBar></SearchBar>
         </nav>
 
     )
